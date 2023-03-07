0862686e435eac5e8775dfe1d0652a127825bfb8
Seongjae <cdaosldk@naver.com>
2023-03-07 10:01:54 +0900

    23/03/07 수정내용 반영

--- src/main/java/com/example/projectblog/config/WebSecurityConfig.java
+++ src/main/java/com/example/projectblog/config/WebSecurityConfig.java
@@ -22,43 +22,46 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableGlobalMethodSecurity(securedEnabled = true) // @Secured 어노테이션 활성화
 public class WebSecurityConfig {
 
-    private final JwtUtil jwtUtil;
+  private final JwtUtil jwtUtil;
 
-    @Bean // Spring Security가 제공하는 적응형 단방향 함수인 bCrypt를 사용해 비밀번호를 암호화
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean // Spring Security가 제공하는 적응형 단방향 함수인 bCrypt를 사용해 비밀번호를 암호화
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
 
-    @Bean // WebSecurityCustomizer는 SecurityFilterChain보다 우선 적용되어 FilterChain을 통한 검사 시 적용된 상태로 검사를 수행
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        // h2-console 사용 및 resources 접근 허용 설정
-        return (web) -> web.ignoring()
-                .requestMatchers(PathRequest.toH2Console())
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
+  @Bean // WebSecurityCustomizer는 SecurityFilterChain보다 우선 적용되어 FilterChain을 통한 검사 시 적용된 상태로 검사를 수행
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    // h2-console 사용 및 resources 접근 허용 설정
+    return (web) -> web.ignoring()
+        .requestMatchers(PathRequest.toH2Console())
+        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+  }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf().disable();
 
-        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        // permitAll()을 사용하여 해당 URL을 인증하지 않고 요청에 응답
-        http.authorizeRequests().antMatchers("/api/user/**").permitAll()
-                .anyRequest().authenticated()
-                // Jwt 토큰을 활용한 인증/인가 설정, CustomFilter 틍록, addFilterBefore(추가할 필터, 추가되는 필터의 다음 필터)
-                // UsernamePasswordAuthenticationFilter : FormLogin 형식을 사용할 때 username과 password를 사용한 인증
-                // 인증에 실패한다면, 기본 로그인 페이지를 반환
-                .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+    // permitAll()을 사용하여 해당 URL을 인증하지 않고 요청에 응답
+    http.authorizeRequests()
+        .antMatchers("/api/user/**").permitAll()
+        .antMatchers("/image-upload").permitAll()
+        .anyRequest().authenticated()
+        // Jwt 토큰을 활용한 인증/인가 설정, CustomFilter 틍록, addFilterBefore(추가할 필터, 추가되는 필터의 다음 필터)
+        // UsernamePasswordAuthenticationFilter : FormLogin 형식을 사용할 때 username과 password를 사용한 인증
+        // 인증에 실패한다면, 기본 로그인 페이지를 반환
+        .and()
+        .addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
-        // 스프링 시큐리티에서 제공하는 기본 FormLogin 방식을 사용하는 로그인페이지의 요청을 인증없이 허용
-        http.formLogin().permitAll();
+    // 스프링 시큐리티에서 제공하는 기본 FormLogin 방식을 사용하는 로그인페이지의 요청을 인증없이 허용
+    http.formLogin().permitAll();
 
-        http.exceptionHandling().accessDeniedPage("/api/user/forbidden");
+    http.exceptionHandling().accessDeniedPage("/api/user/forbidden");
 
-        return http.build();
-    }
+    return http.build();
+  }
 
 }
